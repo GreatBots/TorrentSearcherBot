5ab8abdf36dc7f6649569bd32ce5fc5b93afe69a
Vivek Tp <Vivektvp1@gmail.com>
2023-03-06 15:02:47 +0530

    Update start.py

--- plugins/commands/start.py
+++ plugins/commands/start.py
@@ -5,4 +5,11 @@ from config import START_MESSAGE
 
 @Client.on_message(filters.command("start"))
 async def start_message(Client, Message):
-await message.reply_text(START_MESSAGE)
+await message.reply_text(
+  text=START_MESSAGE,
+  reply_markup=InlineKeyboardMarkup(
+    [
+      InlineKeyboardMarkup("Updates", url="https://t.me/myownbots")
+      ]
+    )
+  )
