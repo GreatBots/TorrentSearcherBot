7acb6e548a87bf516bebdf11ebc4db3fb766a24b
Vivek Tp <Vivektvp1@gmail.com>
2023-03-06 15:14:12 +0530

    Update start.py

--- plugins/commands/start.py
+++ plugins/commands/start.py
@@ -3,6 +3,6 @@ from pyrogram.types import Message
 
 from config import START_MESSAGE
 
-@Client.on_message(pyrogram.Filters.command("start"))
+@Client.on_message(filters.command("start"))
 def start(client, message):
     message.reply(START_MESSAGE)
