652cd4b9857c36da0681360b7bc62f2903370d28
Vivek Tp <Vivektvp1@gmail.com>
2023-03-06 13:37:24 +0530

    Update start.py

--- plugins/commands/start.py
+++ plugins/commands/start.py
@@ -3,7 +3,6 @@ from pyrogram.types import Message
 
 from config import START_MESSAGE
 
-
 @Client.on_message(filters.command("start"))
-async def start_message(c: Client, m: Message):
-    await m.reply_text(START_MESSAGE)
+async def start_message(Client, Message):
+await message.reply_text(START_MESSAGE)
