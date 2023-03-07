bb3a3b7cfafb89727a16b1b75af24a2056f9d1f0
Vivek Tp <Vivektvp1@gmail.com>
2023-03-07 15:44:38 +0530

    Update start.py

--- plugins/commands/start.py
+++ plugins/commands/start.py
@@ -4,6 +4,6 @@ from pyrogram.types import Message
 
 from config import START_MESSAGE
 
-@Client.on_message(pyrogram.Filters.command("start"))
+@Client.on_message(pyrogram.filters.command("start"))
 def start(client, message):
     message.reply("Hi! I'm a Torrent Search Bot for search torrent.I can upload torrent to telegram")
